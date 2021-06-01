faee9a9b6d6a9daaaa8712371ceef24a6f2df601
Bobby Galli <bobbyg603@gmail.com>
2021-06-01 21:02:08 +0000

    Fixes Package can't be imported in Angular project #12 (#13)

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,3 @@
-export { ApiClient } from './api-client';
+export { ApiClient, BugSplatResponse } from './api-client';
 export { BugSplatApiClient } from './bugsplat-api-client';
 export { Environment } from './environment';
--- src/crash/index.ts
+++ src/crash/index.ts
@@ -3,7 +3,7 @@ export { CrashApiClient } from './crash-api-client/crash-api-client';
 export { CrashDetails, DefectTrackerType, ProcessingStatus } from './crash-details/crash-details';
 export { Module } from './module/module';
 export { Register } from './register/register';
-export { StackFrame } from './stack-frame/stack-frame';
+export { StackFrame, VariableValuePair } from './stack-frame/stack-frame';
 export { GroupableThreadCollection } from './thread-collection/groupable-thread-collection';
 export { ThreadCollection } from './thread-collection/thread-collection';
 
