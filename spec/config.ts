a69e55ea1213a2a383e24897a777792cb26fa54b
Bobby Galli <bobbyg603@gmail.com>
2021-09-26 20:48:36 -0400

    Adds support for env variables via dotenv

--- spec/config.ts
+++ spec/config.ts
@@ -1,6 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const host = <string>process.env.BUGSPLAT_HOST;
+const email = <string>process.env.BUGSPLAT_USER;
+const password = <string>process.env.BUGSPLAT_PASSWORD;
+const database = <string>process.env.BUGSPLAT_DATABASE;
+
+if (!host) {
+    throw new Error('Please set BUGSPLAT_HOST env variable');
+}
+
+if (!email) {
+    throw new Error('Please set BUGSPLAT_USER env variable');
+}
+
+if (!password) {
+    throw new Error('Please set BUGSPLAT_PASSWORD env variable');
+}
+
+if (!database) {
+    throw new Error('Please set BUGSPLAT_DATABASE env variable');
+}
+
 export const config = {
-    host: <string>process.env.BUGSPLAT_HOST,
-    email: <string>process.env.BUGSPLAT_USER,
-    password: <string>process.env.BUGSPLAT_PASSWORD,
-    database: <string>process.env.BUGSPLAT_DATABASE
+    host,
+    email,
+    password,
+    database
 };
