a72b4a9f1352ed5b2ea6311f2fdf422a114fb67e
Bobby Galli <bobbyg603@gmail.com>
2021-11-22 08:10:09 -0800

    fix: expose TableDataFormDataBuilder (#61)
    Fixes #60

--- src/common/data/table-data/index.ts
+++ src/common/data/table-data/index.ts
@@ -1,3 +1,4 @@
 export { TableDataClient } from './table-data-client/table-data-client';
 export { TableDataRequest } from './table-data-client/table-data-request';
 export { TableDataResponse } from './table-data-client/table-data-response';
+export { TableDataFormDataBuilder } from './table-data-form-data-builder/table-data-form-data-builder';
