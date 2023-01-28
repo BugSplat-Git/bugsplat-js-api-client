f9509a1b66e031b4fc539d538ed5a477730cb838
Bobby Galli <bobbyg603@gmail.com>
2023-01-28 15:23:57 -0500

    chore: fix exports

--- src/crashes/index.ts
+++ src/crashes/index.ts
@@ -1,3 +1,4 @@
 export { CrashesApiClient } from './crashes-api-client/crashes-api-client';
 export { CrashesApiRow } from './crashes-api-row/crashes-api-row';
 export { KeyCrashApiClient } from './key-crash-api-client/key-crash-api-client';
+export { KeyCrashTableDataRequest } from './key-crash-api-client/key-crash-table-data-request';
