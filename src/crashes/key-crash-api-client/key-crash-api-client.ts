0edbdc16493f0a6a5e7ea07026225e1c11cbb001
Bobby Galli <bobbyg603@gmail.com>
2023-01-30 22:25:13 -0500

    feat: add key crash page data

--- src/crashes/key-crash-api-client/key-crash-api-client.ts
+++ src/crashes/key-crash-api-client/key-crash-api-client.ts
@@ -1,6 +1,7 @@
 import { ApiClient, BugSplatResponse, TableDataClient, TableDataResponse } from '@common';
 import { CrashesApiRow } from '@crashes';
 import { CrashesApiResponseRow } from '../crashes-api-row/crashes-api-row';
+import { KeyCrashPageData } from './key-crash-page-data';
 import { KeyCrashTableDataRequest } from './key-crash-table-data-request';
 
 export class KeyCrashApiClient {
@@ -11,9 +12,9 @@ export class KeyCrashApiClient {
         this._tableDataClient = new TableDataClient(this._client, '/keycrash?data&crashTimeSpan');
     }
 
-    async getCrashes(request: KeyCrashTableDataRequest): Promise<TableDataResponse<CrashesApiRow>> {
+    async getCrashes(request: KeyCrashTableDataRequest): Promise<TableDataResponse<CrashesApiRow, KeyCrashPageData>> {
         const formParts = { stackKeyId: `${request.stackKeyId}` };
-        const response = await this._tableDataClient.postGetData<CrashesApiResponseRow>(request, formParts);
+        const response = await this._tableDataClient.postGetData<CrashesApiResponseRow, KeyCrashPageData>(request, formParts);
         const json = await response.json();
         const pageData = json.pageData;
         const rows = json.rows.map(row => new CrashesApiRow(row));
