20ca0e77c7d470400e511c9eace1f15a0228021e
Bobby Galli <bobbyg603@gmail.com>
2023-01-30 22:24:56 -0500

    feat: add crashes page data

---
+++ src/crashes/crashes-api-client/crashes-page-data.ts
@@ -0,0 +1,6 @@
+import { DefectTrackerType } from '@crash';
+
+export interface CrashesPageData {
+    defectTracker: boolean;
+    defectTrackerType: DefectTrackerType;
+}
--- src/crashes/index.ts
+++ src/crashes/index.ts
@@ -1,4 +1,5 @@
 export { CrashesApiClient } from './crashes-api-client/crashes-api-client';
+export { CrashesPageData } from './crashes-api-client/crashes-page-data';
 export { CrashesApiRow } from './crashes-api-row/crashes-api-row';
 export { KeyCrashApiClient } from './key-crash-api-client/key-crash-api-client';
 export { KeyCrashTableDataRequest } from './key-crash-api-client/key-crash-table-data-request';
