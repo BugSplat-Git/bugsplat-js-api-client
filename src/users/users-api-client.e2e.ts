5ffe5dd15be0415402efa3ba350e7e95e49ee26f
Bobby Galli <bobbyg603@gmail.com>
2024-01-16 11:43:39 -0500

    chore: user e2e test

--- src/users/users-api-client.e2e.ts
+++ src/users/users-api-client.e2e.ts
@@ -1,14 +1,17 @@
-import { BugSplatApiClient } from '@common';
+import { BugSplatApiClient, TableDataResponse } from '@common';
 import { config } from '@spec/config';
 import { UserApiResponseStatus, UsersApiClient } from './users-api-client';
 
 describe('UsersApiClient', () => {
+    let companyId: number;
     let testEmail: string;
     let usersClient: UsersApiClient;
+
 
     beforeEach(async () => {
         const { host, email, password } = config;
         const bugsplat = await BugSplatApiClient.createAuthenticatedClientForNode(email, password, host);
+        companyId = await getCompanyId(bugsplat, config.database);
         usersClient = new UsersApiClient(bugsplat);
         testEmail = 'bobby+unittests@bugsplat.com';
     });
@@ -70,5 +73,31 @@ describe('UsersApiClient', () => {
         });
     });
 
-    // TODO BG companies tests
+    describe('addUserToCompany', () => {
+        it('should return 200 and message', async () => {
+            const response = await usersClient.addUserToCompany(companyId, testEmail);
+            const body = await response.json();
+            expect(response.status).toEqual(200);
+            expect(body.status).not.toEqual(UserApiResponseStatus.fail);
+        });
+    });
+
+    describe('removeUserFromCompany', () => {
+        it('should return 200 and message', async () => {
+            const { uId: uIdAdded } = await await usersClient.addUserToCompany(companyId, testEmail).then(response => response.json());
+            const response = await usersClient.removeUserFromCompany(companyId, uIdAdded);
+            const body = await response.json();
+            expect(response.status).toEqual(200);
+            expect(body.status).not.toEqual(UserApiResponseStatus.fail);
+        });
+    });
 });
+
+async function getCompanyId(apiClient: BugSplatApiClient, database: string): Promise<number> {
+    const rows = await apiClient.fetch<Array<{ dbName: string, companyId: string }>>('/api/databases.php').then(response => response.json());
+    const row = rows.find(row => row.dbName === database);
+    if (!row?.companyId) {
+        throw new Error(`Could not find database ${database}`);
+    }
+    return Number(row.companyId);
+}
