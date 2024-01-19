afcb629653a42f10559b933d5777aa1ec50a8c03
Bobby Galli <bobbyg603@gmail.com>
2024-01-19 11:48:21 -0500

    fix: mac crashTypeId (#118)

--- src/post/crash-type.ts
+++ src/post/crash-type.ts
@@ -3,5 +3,6 @@ export class CrashType {
     static readonly dotnet = new CrashType('Windows.NET', 8);
     static readonly electron = new CrashType('Electron', 22);
     static readonly native = new CrashType('Windows.Native', 1);
+    static readonly mac = new CrashType('macOS', 13);
     private constructor(public readonly name: string, public readonly id: number) { }
 }
