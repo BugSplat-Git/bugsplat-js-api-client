b2c9c5086c11469dd2e2d3ebc0ff42ec2a7471bf
Bobby Galli <bobbyg603@pm.me>
2025-10-30 20:17:35 -0400

    chore: export ErrorResponse from table-data

--- src/common/data/table-data/index.ts
+++ src/common/data/table-data/index.ts
@@ -1,4 +1,4 @@
-export { TableDataClient, isErrorResponse } from './table-data-client/table-data-client';
+export { TableDataClient, isErrorResponse, ErrorResponse } from './table-data-client/table-data-client';
 export { TableDataRequest } from './table-data-client/table-data-request';
 export { TableDataResponse } from './table-data-client/table-data-response';
 export { ColumnSortOrder } from './table-data-form-data-builder/column-sort-order';
